126406e0d75be82186f0e5de6b092f7049114fb8
yandutka@mail.ru <yandutka@mail.ru>
2021-11-12 20:15:39 +0300

    delegation DONE

--- String/String.cpp
+++ String/String.cpp
@@ -32,7 +32,7 @@ public:
 	{
 		cout << "DefaultConstructor:\t" << this << endl;
 	}
-	String(const char* str):size(strlen(str)+1), str(new char[size]{})
+	String(const char* str):String(strlen(str) + 1)  //Делегируем выделение памяти конструктору по умолчанию
 	{
 		for (int i = 0; i < size; i++)
 		{
@@ -41,13 +41,8 @@ public:
 		cout << "Constructor:\t\t" << this << endl;
 	}
 	//         The rule of three
-	String(const String& other):size(other.size), str(new char[size] {})
+	String(const String& other):String(other.str)
     {
-		//Deep copy
-		for (int i = 0; i < size; i++)
-		{
-			this->str[i] = other.str[i];
-		}
 		cout << "CopyConstructor:\t" << this << endl;
 	}
 	String(String&& other):size(other.size), str(other.str)
